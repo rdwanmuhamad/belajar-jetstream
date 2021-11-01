d14beca9b438d87dfd57b507dc746c3f64eb0632
rdwanmuhamad <mrdwn624@gmail.com>
2021-11-01 09:46:12 +0800

    membuat create view

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -13,31 +13,53 @@
                     Create Todo
                 </a>
             </div>
-            <table class="table-auto w-full">
-                <tr>
-                    <th class="border px-4 py-2 width:10%">ID</th>
-                    <th class="border px-4 py-2 width:70%">Todo</th>
-                    <th class="border px-4 py-2 width:20%">Aksi</th>
-                </tr>
-            @forelse ($todos as $todo)
-                <tr>
-                    <td class="border px-4 py-2 width:10%">{{ $todo->id }}</td>
-                    <td class="border px-4 py-2 width:70%">{{ $todo->todo }}</td>
-                    <td class="border px-4 py-2 width:20%">
-                        <a href="{{ route('todo.edit', $todo->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</a>
-                        <form action="{{ route('todo.destroy', $todo->id) }}" method="POST" class="inline-block">
-                            @csrf
-                            @method('delete')
-                            <button type="submit">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="3" class="border px-4 py-2 text-center">Tidak Ada Data</td>    
-                </tr>    
-            @endforelse
-            </table>
+            <div class="flex flex-col pt-5">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                ID
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Title
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Action
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @forelse ($todos as $todo)
+                            <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm font-medium text-gray-900">{{ $todo->id }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{ $todo->todo }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <a href="{{ route('todo.edit', $todo->id) }}" class="mr-3 text-indigo-600 hover:text-indigo-900">Edit</a>
+                                <form action="{{ route('todo.destroy', $todo->id) }}" method="POST" class="inline-block ">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="text-indigo-600 hover:text-indigo-900">Delete</button>
+                                </form>
+                            </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="3" class="border px-4 py-2 text-center">Tidak Ada Data</td>    
+                            </tr>    
+                            @endforelse
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
